e1fa8bf6a0be96aa74f7cae49a7853e9ea6df2ab
“Breno <brenorodriguesdev@gmail.com>
2021-11-29 18:08:04 -0300

    feat: add farmacia model

---
+++ src/domain/models/farmacia.ts
@@ -0,0 +1,11 @@
+export interface FarmaciaModel {
+    id?: number
+    logo: string
+    nome: string
+    cnpj: string
+    endereco: string
+    horarioFuncionamento: string
+    responsavel: string
+    telefone: string
+    outros?: string
+}
