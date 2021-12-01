8af42994c3a8434b4920b29b03b22df96daf1bee
“Breno <brenorodriguesdev@gmail.com>
2021-11-30 22:15:53 -0300

    feat: add farmacia entity

--- src/data/entities/farmacia.ts
+++ src/data/entities/farmacia.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
 
 @Entity('farmacia')
 export class Farmacia {
