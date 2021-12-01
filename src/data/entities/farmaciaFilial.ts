61a4286764d0764f803b598ee22b86e2170f0d5b
“Breno <brenorodriguesdev@gmail.com>
2021-11-30 22:16:05 -0300

    feat: add farmacia filial entity

---
+++ src/data/entities/farmaciaFilial.ts
@@ -0,0 +1,18 @@
+import { Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { Farmacia } from './farmacia'
+import { FarmaciaSede } from './farmarciaSede'
+
+@Entity('farmaciaFilial')
+export class FarmaciaFilial {
+  @PrimaryGeneratedColumn('increment')
+  id: number
+
+  @OneToOne(() => Farmacia)
+  @JoinColumn({ name: 'idFarmacia' })
+  farmacia: 
+
+  @ManyToOne(() => FarmaciaSede, farmaciaSede => farmaciaSede.filias)
+  @JoinColumn({ name: 'idFarmaciaFilial' })
+  farmaciaSede: FarmaciaSede
+
+}
