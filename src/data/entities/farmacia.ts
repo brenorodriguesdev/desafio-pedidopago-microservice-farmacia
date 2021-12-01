1903fc1e30d190d2f6977595ec0c57285e5d6674
“Breno <brenorodriguesdev@gmail.com>
2021-11-30 21:51:58 -0300

    feat: add farmacia entity

---
+++ src/data/entities/farmacia.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity('farmacia')
+export class Farmacia {
+  @PrimaryGeneratedColumn('increment')
+  id: number
+
+  @Column()
+  logo: string
+
+  @Column()
+  nome: string
+
+  @Column()
+  cnpj: string
+
+  @Column()
+  endereco: string
+
+  @Column()
+  horarioFuncionamento: string
+
+  @Column()
+  responsavel: string
+
+  @Column()
+  telefone: string
+
+  @Column()
+  outros: string
+}
